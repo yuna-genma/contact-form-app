9f3c4c0b26464e0182452909e8e6fa3f66966c59
YunaGenma <maeyf16@icloud.com>
2026-08-15 18:14:27 +0900

    feat:認証機能の実装 #5

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ContactController::class, 'index'])->name('form.show');
 Route::post('/contacts/confirm', [ContactController::class, 'store'])->name('form.confirm');
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('form.thanks');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', fn() => '管理画面（準備中）');
+    Route::get('/admin/contacts/{contact}', fn() => 'お問い合わせ詳細ページ（準備中）');
+    Route::get('/admin/tags/{tag}/edit', fn() => 'タグ編集ページ（準備中）');
+});
