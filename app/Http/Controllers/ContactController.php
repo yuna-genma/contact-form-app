1224bed0cb02c3fb3a26bb874a57967c030152a5
YunaGenma <maeyf16@icloud.com>
2026-08-15 22:11:49 +0900

    feat:タグCRUD機能実装 #6

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,21 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreContactRequest;
 use App\Models\Contact;
 use App\Models\Category;
+use App\Models\Tag;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        $categories = Category::orderBy('created_at', 'desc')->get();
-        return view('contact.index', compact('categories'));
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('contact.index', compact(['categories', 'tags']));
     }
-
     public function store(StoreContactRequest $request)
     {
-        $validated = Contact::create($request->validated());
-        $category = Category::find($validated->category_id);
+        $contactData = $request->validated();
 
-        return view('contact.confirm', compact(['validated', 'category']));
+        $tagIds = $contactData['tag_ids'] ?? [];
+
+        $contact = $contactData;
+        unset($contact['tag_ids']);
+
+        $validated = Contact::create($contact);
+        $validated->tags()->attach($tagIds);
+
+        $category = Category::find($validated->category_id);
+        $tags = Tag::whereIn('id', $tagIds)->get();
+
+        return view('contact.confirm', compact(['validated', 'category', 'tags']));
     }
 
     public function thanks()
---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateTagRequest;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    public function store(UpdateTagRequest $request)
+    {
+        $validated = $request->validated();
+        Tag::create($validated);
+
+        return redirect('/admin');
+    }
+    public function edit(Tag $tag)
+    {
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $tag->update($request->validated());
+        return redirect('/admin');
+    }
+
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        return redirect('/admin');
+    }
+}
