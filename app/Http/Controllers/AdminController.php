7ae683c26f836ed692d844dae81942a1bb7a145b
YunaGenma <maeyf16@icloud.com>
2026-08-30 18:16:15 +0900

    Fixed AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
 {
     public function index(IndexContactRequest $request)
     {
-        $query = Contact::query();
+        $query = Contact::with(['category', 'tags']);
 
         if ($request->filled('keyword')) {
             $keyword = $request->input('keyword');
